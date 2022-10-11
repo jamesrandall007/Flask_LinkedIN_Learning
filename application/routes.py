859312ab8cd594b5ebd543d35bdbf9b87c1920fb
James <james@medzec.com>
2022-10-11 09:05:11 -0700

    First page working and functioning

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,17 @@
 from application import app
 from flask import render_template
+import os
+
+server_status = os.getenv('FLASK_ENV')
 
 
 @app.route('/')
 @app.route("/index")
 def index():
-    return "<h1>I am here, from {{testing_count}} James C {{FLASK_DEBUG}}  Randall. app ini no Debug is active</h1>"
+    return render_template("index.html")
+
+
+@app.route('/james')
+def james():
+    return f"<H1>Hi JAMES</H1>" \
+           f"the system is in {server_status} MODE "
